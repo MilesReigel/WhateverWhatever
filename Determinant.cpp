0356cc267a98064db4148fc2fe5d6d78d59660fa
RocketMR-CU <mire4025@colorado.edu>
2025-08-31 23:34:08 -0600

    1

--- Determinant.cpp
+++ Determinant.cpp
@@ -4,15 +4,60 @@ using namespace std;
 
 // Take determinant of array
 // Create string output
-
 // Take vector coefficient inputs,
-// Arrange into an array
-void input()
+// Math and ouput
+
+void input2x2()
+{
+    int coef2x2[2][2];
+    for (int i = 0; i < 4; i++)
+    {
+        cout << "Input coefficients one at a time, left to right and top to bottom: " << endl;
+        if (i < 2)
+        {
+            cin >> coef2x2[0][i];
+        }
+        else
+        {
+            cin >> coef2x2[1][i];
+        }
+    }
+    int output = ((coef2x2[0][0] * coef2x2[1][1]) - (coef2x2[0][1] * coef2x2[1][0]));
+    cout << output;
+}
+
+void input3x3()
+{
+    int coef[3][3];
+    for (int i = 0; i < 9; i++)
+    {
+        cout << "Input coefficients one at a time, left to right and top to bottom: " << endl;
+        if (i < 3)
+        {
+            cin >> coef[0][i];
+        }
+        else if (3 < i < 7)
+        {
+            cin >> coef[1][i - 3];
+        }
+        else
+        {
+            cin >> coef[2][i-6];
+        }
+    }
+    int x = (coef[0][0])*(((coef[1][1])*(coef[2][2])) - (coef[1][2])*(coef[2][1]));
+    int y = (coef[0][1])*(((coef[1][2])*(coef[2][0])) - (coef[1][0])*(coef[2][2]));
+    int z = (coef[0][2])*(((coef[1][0])*(coef[2][1])) - (coef[1][1])*(coef[2][0]));
+    int output = x + y + z;
+    cout << output;
+}
+
+void inputCross()
 {
     int coef[2][3];
     for (int i = 0; i < 6; i++)
     {
-        cout << "Input coefficient " << i + 1 << ": " << endl;
+        cout << "Input coefficients one at a time, left to right and top to bottom: " << endl;
         if (i < 3 )
         {
             cin >> coef[0][i];
@@ -26,13 +71,29 @@ void input()
     int jcoef = ((coef[0][2])*(coef[1][0])-(coef[0][0])*(coef[1][2]));
     int kcoef = ((coef[0][0])*(coef[1][1])-(coef[0][1])*(coef[1][0]));
 
-    cout << "Your determinant is:" << endl;
+    cout << "Your cross-product is: ";
     cout << icoef << "X + " << jcoef << "Y + " << kcoef << "Z." << endl;
 }
 
 int main()
 {
-    input();
+    string choice;
+    cout << "What kind of determinant would you like to take?" << endl; 
+    cout << "The options are A)2x2, B)3x3, or C)Vector Cross Product" << endl;
+    cin >> choice;
+
+    if (choice == "a" || "A")
+    {
+        input2x2();
+    } 
+    else if (choice == "b" || "B")
+    {
+        input3x3();
+    }
+    else
+    {
+        inputCross();
+    }
 
     return 0;
 }
