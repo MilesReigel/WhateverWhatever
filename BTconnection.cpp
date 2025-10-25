97e07f0983ca82c0e1c7e24d96448955106af056
RocketMR-CU <mire4025@colorado.edu>
2025-10-25 14:11:13 -0600

    1

--- BTconnection.cpp
+++ BTconnection.cpp
@@ -1,6 +1,9 @@
 #include <BluetoothSerial.h>
 #include <ESP32Servo.h>
 #include <Stepper.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to enable it
@@ -8,46 +11,88 @@
 
 BluetoothSerial SerialBT;
 Servo myservo;
-//Stepper myStepper(2048, 22, 21, 19, 18);
-int servoPos;
-static const int servoPin = 4;
+char c;
+static const int servoPin = 14;
+int theta = 0, rho = 0, SPin1 = 22, SPin2 = 21, SPin3 = 19, SPin4 = 18;
+bool autorunTF = 0;
+String input;
+Stepper mystepper(2048, SPin1, SPin2, SPin3, SPin4);
 
 
+void Clockwise() {
+  SPin1 = 22;
+  SPin2 = 21;
+  SPin3 = 19;
+  SPin4 = 18;
+}
 
-void setup() 
-{
+void Counterclockwise() {
+  SPin1 = 18;
+  SPin2 = 19;
+  SPin3 = 21;
+  SPin4 = 22;
+}
+
+void autoRun() {
+  srand(time(NULL));
+  rho = (rand() + 10) % 160 + 10;
+  theta = rand() % 360;
+
+  myservo.write(rho);
+  mystepper.setSpeed(10);
+  if (theta >= 180) {
+    Clockwise();
+  } else {
+    Counterclockwise();
+  }
+  mystepper.step((2048 / 360) * (theta / 2));
+  delay(2000);
+}
+
+bool inputIsNumber(String inputStr) {
+  for (char c : inputStr) {
+    if (!(48 <= c && 57 >= c)) {
+      return (0);
+    } else {
+      return (1);
+    }
+  }
+}
+
+int strtoint(String str) {
+  int intconverter = (100 * int(str[0] - '0')) + (10 * int(str[1] - '0')) + int(str[2] - '0');
+  if (intconverter > 0 && intconverter < 180) {
+    return (intconverter);
+  } else {
+    SerialBT.println("Invalid Input");
+    return (0);
+  }
+}
+
+void setup() {
   Serial.begin(115200);
-  SerialBT.begin("Playstation 6");
-  Serial.println("The device has started, bluetooth pairing is enabled");
+  SerialBT.begin("ESP32TEST");
 
   myservo.attach(servoPin, 500, 2400);
   myservo.setPeriodHertz(50);
 }
 
-void loop() 
-{
-  //myStepper.setSpeed(10);
-  //myStepper.step(2048);
-  //delay(1000);
-
-  if (SerialBT.available()) 
-  {
-    Serial.write(SerialBT.read());
+void loop() {
+  if (SerialBT.available()) {
+    input = SerialBT.readStringUntil('\n');
+    c = input[0];
+    if (c == 'a' || c == 'A') {
+      SerialBT.println("Automatic Mode Activated");
+      autorunTF = 1;
+    } else if (c == 'm' || c == 'M') {
+      SerialBT.println("Manual Mode Activated");
+    }
+    if (inputIsNumber(input)) {
+      myservo.write(strtoint(input));
+    }
   }
-  //myservo.writeMicroseconds(servoPos);
-  
-  /*
-  for (int i = 0; i <= 180; i++)
-  {
-    myservo.write(i);
-    SerialBT.println(i);
-    delay(20);
+  if (autorunTF) {
+    autoRun();
   }
-  for (int i = 180; i >= 0; i--)
-  {
-    myservo.write(i);
-    SerialBT.println(i);
-    delay(20);
-  }
-  */
+  delay(20);
 }
