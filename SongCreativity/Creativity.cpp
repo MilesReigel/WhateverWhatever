a3ba8cb154f11f8dc37d1c83bb778aa0ae8dbc00
RocketMR-CU <mire4025@colorado.edu>
2026-02-10 16:47:33 -0700

    commit

--- SongCreativity/Creativity.cpp
+++ SongCreativity/Creativity.cpp
@@ -3,14 +3,29 @@
 #include <vector>
 
 using namespace std;
+//declaration of independence
+int indexOfLargest = 0, totalwords = 0;
+string line, tempword;
+vector<string> wordvector;
+vector<int> wordcounts;
+
+char lowercaseify(char c) {
+    if (int(c) > 64 && int(c) < 91) {
+        return(char(int(c) + 32));
+    }
+    else {
+        return(c);
+    }
+}
+
+void putitinthere(string& inputword, vector<string>& wordvec, vector<int>& countvec) {
+    wordvec.push_back(inputword);
+    countvec.push_back(1);
+    totalwords++;
+    inputword.clear();
+}
 
 int main() {
-    string line, word;
-    bool existsAlready = false;
-    vector<string> words;
-    vector<int> wordcounts;
-    int indexOfLargest = 0;
-
     ifstream file("song.txt");
 
     if (file.fail()) {
@@ -18,32 +33,46 @@ int main() {
     }
 
     while (getline(file, line)) {
-        for (int i = 0; i < line.length(); i++) {
-            if (line[i] != ' ') {
-                word += line[i];
+        for (char c : line) {
+            if ((c != ' ') && (c != '(') && (c != ')') && (c != ',') && (c != '\r')) {
+                tempword += lowercaseify(c);
             }
             else {
-                for (int i = 0; i < words.size(); i++) {
-                    if (words[i] == word) {
-                        existsAlready = true;
-                        wordcounts[i]++;
-                    }
+                if (wordvector.empty()) {
+                    putitinthere(tempword, wordvector, wordcounts);
                 }
-                if (!existsAlready) {
-                    words.push_back(word);
-                    wordcounts.push_back(1);
-                    word = "";
-                    existsAlready = false;
+                
+                if (!tempword.empty()) {
+                    bool found = false;
+                    for (int i = 0; i < totalwords; i++) {
+                        if (wordvector[i] == tempword) {
+                            wordcounts[i]++;
+                            found = true;
+                            tempword.clear();
+                            break;
+                        }
+                    }
+                    if (!found) {
+                        putitinthere(tempword, wordvector, wordcounts);
+                    }
                 }
             }
         }
+        tempword.clear();
     }
-    for (int i : wordcounts) {
+
+    for (int i = 0; i < totalwords; i++) {
         if (wordcounts[i] > wordcounts[indexOfLargest]) {
             indexOfLargest = i;
         }
     }
-    cout << "The most common word is " << words[indexOfLargest] << " at " << wordcounts[indexOfLargest] << " times." << endl;
 
+    cout << "There are " << totalwords << " words in the song" << endl;
+    cout << "The most common word is \"" << wordvector[indexOfLargest] << "\" at " << wordcounts[indexOfLargest] << " times." << endl << endl;
+
+    for (int i = 0; i < totalwords; i++) {
+        cout << i + 1 << ": \"" << wordvector[i] << "\" - " << wordcounts[i] << endl;
+    }
+    
     return(0);
 }
