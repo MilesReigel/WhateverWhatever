7b0fee933dccb3cd7ed5eae038cfde80277099ea
MilesReigel <miles.reigel@gmail.com>
2026-05-30 20:30:23 -0600

    tweaked symreg constraints

--- Regression/practice_3.py
+++ Regression/practice_3.py
@@ -3,24 +3,43 @@
 import numpy as np
 from pysr import PySRRegressor
 import matplotlib.pyplot as plt
+import time
 
-X = 3 * np.random.randn(50, 7)
-y = (8.445 * np.sin(X[:, 5])) - (X[:, 3] ** 4) + (3 / X[:, 0])
+start_time = time.perf_counter()
+print(f"PySR optimization started at time stamp{start_time: .3f}")
+
+X = 3 * np.random.randn(50, 6)
+y = (8.445 * np.sin(X[:, 5])) - (X[:, 3] ** 4) + (3 / X[:, 0]) + 4
 
 model = PySRRegressor(
-    ncycles_per_iteration = 100,
+    # warm_start = True,
     niterations = 10000000,
-    early_stop_condition = ("stop_if(loss, complexity) = loss < 1 && complexity < 10"),
+    population_size = 50,
+    populations = 8,
+    maxsize = 40,
+    maxdepth = 5,
+    weight_randomize = 0.1,
+    ncycles_per_iteration = 400,
+    complexity_of_constants = 2,
+    early_stop_condition = ("stop_if(loss, complexity) = (complexity < 21 && loss < 1e-10) || (complexity < 16 && loss < 1e-3)"),
     timeout_in_seconds = 3600,
     binary_operators = ["+", "-", "*", "/"],
-    unary_operators = ["cos", "sin", "exp", "inv(x) = 1/x", "square", "cos2(x) = cos(x)^2",],
+    unary_operators = ["cos", "sin", "exp", "square",],
     constraints = {"square": 5, "cube": 5, "exp": 5,},
-    select_k_features = 4,
+    nested_constraints = {
+        "square": {"square": 1, "sin": 2, "cos": 2, "exp": 1},
+        "sin": {"square": 1, "sin": 2, "cos": 2, "exp": 1},
+        "cos": {"square": 1, "sin": 2, "cos": 2, "exp": 1},
+        "exp": {"square": 1, "sin": 2, "cos": 2, "exp": 1},},
     turbo = True,
-    extra_sympy_mappings = {"inv": lambda x: 1/x, "cos2": lambda x: sympy.cos(x) ** 2},
-    elementwise_loss = "loss(prediction, target) = (prediction - target)^2",
+    elementwise_loss = "loss(prediction, target) = (prediction - target)^4",
 )
-model.reset()
+
 model.fit(X, y)
+
+end_time = time.perf_counter()
+exec_time = end_time - start_time
+print(f"Finished execution at{end_time: .3f}")
+print(f"Execution time: {exec_time: .3f}s")
 
 print(model)
