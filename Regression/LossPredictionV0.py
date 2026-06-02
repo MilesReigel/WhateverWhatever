74df825c872f6960950496d0ce94bf7fb9db87be
MilesReigel <mire4025@colorado.edu>
2026-06-02 14:10:13 -0600

    git test

--- Regression/LossPredictionV0.py
+++ Regression/LossPredictionV0.py
@@ -6,7 +6,7 @@ import h5py
 
 # Notes:
 # Data sampling time is consistently 8ns
-T_interval = 8e-9 #seconds
+T_interval = 8e-9 #seconds, 8 ns
 
 # Discarding_info is 128x1, data included is of unknown purpose
 # Acquisition info is 294x1, similarly of unknown purpose
@@ -21,11 +21,12 @@ with h5py.File('3C90_Pretest_data/3C90_TX-25-15-10_Data1_Combined.mat', 'r') as 
             for outer in temp: # Arrays are occasionally mismatching in terms of row/columnization
                 for inner in outer:
                     str_assembly += chr(inner)
-            print(f"(Key: {key}): {str_assembly}")
+            print(f"{key}: {str_assembly}")
         elif temp.shape == (1, 1):
             print(f"{key}: {temp[0][0]}")
         else:
             print(temp)
 
-    freq_data = Data_raw['Frequency_command']
-    print(freq_data[:])
+    time_data = Data_raw['Sampling_Time']
+
+    
