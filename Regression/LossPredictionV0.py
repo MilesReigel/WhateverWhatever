601fe3d3f88d71941496e4341665a32ce6600b22
MilesReigel <mire4025@colorado.edu>
2026-06-08 21:05:25 -0600

    update

--- Regression/LossPredictionV0.py
+++ Regression/LossPredictionV0.py
@@ -5,24 +5,36 @@ from scipy.signal import find_peaks, detrend
 import numpy as np
 import h5py
 
-#figure out fft of data, complete B, H and mu functions, figure out how to group SR batches/ downsample data
+# Start with 3 fourier terms, advance if greater accuracy is needed
 
-def B_t(A, N2, V_t):
-    B_t = np.zeros(1024)
-    for i in range(1024):
-        B_t[i] = V_t[0]/(A * N2) if i == 0 else B_t[i-1] + V_t[i]/(A * N2)
+
+def B_t(A, N2, V_t, sampling_time):
+    dt = sampling_time / 1024
+    B_t = np.cumsum(V_t) * dt / (A * N2)
     return B_t
 
-def fft_B(A, N2, data): # test
-    for f, a, p in data:
-        a /= (2 * np.pi * f * A * N2)
-        p -= np.pi / 2
+def B_from_fft(A, N2, data, sampling_time): # returns (frequency, amplitude, phase, dc offset) of B_t
+    # 1/(A * N2) INT(V_t) = 1/(A * N2) * INT(a * cos(wt + p)) = 1/(A * N2) * a/w * sin(wt + p) = 1/(A * N2) * a/w * cos(wt + p + pi/2) -> a /= w*A*N2, f = f, p += pi/2
+    data_out = []
+    for f, a, phase in data:
+        new_a = a / (2 * np.pi * f * A * N2)
+        new_p = phase - (np.pi / 2)
+        dc_offset = -new_a * np.cos((f * 2 * np.pi * sampling_time / 1024) + new_p)
+        data_out.append((f, new_a, new_p, dc_offset))
+    return data_out
 
 def H_t(L, N1, I_t):
     H_t = np.zeros(1024)
     for H, I in zip(H_t, I_t):
         H =  I * (N1 / L)
     return H_t
+
+def H_from_fft(L, N1, data): # returns (frequency, amplitude, phase, 0) of H_t
+    data_out = []
+    for f, a, phase in data:
+        new_a = a * N1 / L
+        data_out.append((f, new_a, phase, 0))
+    return data_out
 
 def mu_a(B_t, H_t):
     return (max(B_t) - min(B_t))/(max(H_t) - min(H_t))
@@ -31,10 +43,11 @@ def get_data(filename): # pull necessary data from the file, store to object and
     data = h5py.File(filename, 'r')
     return data['Data']
 
-def ship_of_theseus(sampling_time, wave, n, is_B): # returns (frequency, amplitude, phase) of n most prominent components of given wave
+def ship_of_theseus(sampling_time, wave, n): # returns (frequency, amplitude, phase) of n most prominent components of given wave
     x = fftfreq(1024, sampling_time / 1024)[:512]
     y = fft(wave)
     amplitudes = np.abs(y[:512]) / 512
+    amplitudes[x < 1000] = 0.0 # filter any low frequency fft products out
     phases = np.angle(y[:512])
     frequency_idxs, _ = find_peaks(amplitudes, height = 0.01 * max(amplitudes))
     peak_amps = amplitudes[frequency_idxs]
@@ -46,12 +59,13 @@ def ship_of_theseus(sampling_time, wave, n, is_B): # returns (frequency, amplitu
 
 def recombobulate(time, data):
     combobulated_wave = np.zeros(1024)
-    for f, a, phase in data:
-        combobulated_wave += a * np.cos(2 * time * f * np.pi + phase)
-    return combobulated_wave # + (max(combobulated_wave) - min(combobulated_wave)) / 2
+    for f, a, phase, dc in data:
+        combobulated_wave += a * np.cos(2 * time * f * np.pi + phase) + dc
+    return combobulated_wave
+
 
 class Materials:
-    def __init__(self, V, I, DutyN, DutyP, Temp, Flux, Freq, Hdc, Area, N1, N2, samples, sampling_time):
+    def __init__(self, V, I, DutyN, DutyP, Temp, Flux, Freq, Hdc, Area, L, N1, N2, samples, sampling_time):
         self.V = V
         self.I = I
         self.duty_dif = np.zeros((samples), dtype = 'f4')
@@ -60,6 +74,7 @@ class Materials:
         self.Freq = np.array(Freq[0, :], dtype = 'f8')
         self.Hdc = np.array(Hdc[0, :], dtype = 'f8')
         self.Area = Area[0, 0]
+        self.L = L[0, 0]
         self.N1 = N1[0, 0]
         self.N2 = N2[0, 0]
         self.B_t = np.zeros(samples, dtype = 'f8')
@@ -67,11 +82,11 @@ class Materials:
         self.samples = samples
         self.sampling_time = sampling_time[0, :]
 
-        for i in range(samples): # clean duty cycle for regression processing and create a ratio to reduce memory usage
+        for i in range(samples): # clean duty cycle for regression processing and create a ratio to reduce memory demand
             self.duty_dif[i] = 1.0 if np.isnan(DutyN[0, i]) else DutyP[0, i] - DutyN[0, i]
         
 
-print("Organizing Data . . .")
+print("Organizing Data. . .")
 Data_raw = get_data('3C90_Pretest_data/3C90_TX-25-15-10_Data1_Cycle.mat')
 for key in Data_raw.keys():
     temp = Data_raw[key]
@@ -97,22 +112,19 @@ m1 = Materials(
     Data_raw['Frequency_command'],
     Data_raw['Hdc_command'],
     Data_raw['Effective_Area'],
+    Data_raw['Effective_Length'],
     Data_raw['Primary_Turns'],
     Data_raw['Secondary_Turns'],
     len(Data_raw['Sampling_Time'][0]),
     Data_raw['Sampling_Time'])
 del Data_raw
 
-index = 12000
-temp_time = m1.sampling_time[index]
-t_series = np.arange(0, temp_time, temp_time/1024)
-B_ifft_fft_V_t = recombobulate(t_series, ship_of_theseus(temp_time, m1.V[:, index], 20)) # B_t(m1.Area, m1.N2, m1.V[:, index])
-ifft_B_fft_V_t = recombobulate(t_series, ship_of_theseus(temp_time, B_t(m1.Area, m1.N2, m1.V[:, index]), 20))
-plt.plot(t_series, B_t(m1.Area, m1.N2, B_fft_V_t), label = "B(fft(V(t)))", color = 'b')
-plt.plot(t_series, fft_B_V_t, label = "fft(B(V(t)))", color = 'r')
-plt.plot(t_series, B_t(m1.Area, m1.N2, m1.V[:, index]), label = "B(V(t))", color = 'k')
-plt.legend()
-plt.savefig('temp')
+
+print("Computing Losses. . .")
+for Vcycle, Icycle, time in zip(m1.V, m1.I, m1.sampling_time):
+    m1.B_t = B_from_fft(m1.Area, m1.N2, ship_of_theseus(time, Vcycle, 3), time)
+    m1.H_t = H_from_fft(m1.L, m1.N1, ship_of_theseus(time, Icycle, 3))
+del m1.V, m1.I
 
 
 # temp_points = [25.0, 50.0, 70.0, 90.0]
@@ -122,17 +134,29 @@ for i, t in enumerate(m1.Temp):
         groups[t][1] += 1 # size of group
         groups[t][0] = i if groups[t][0] is None else groups[t][0] # start index
 
+r1 = groups[25.0][0]
+r2 = groups[50.0][0]
 
-xdata = np.column_stack((
-    m1.Temp[groups[25.0][0]:groups[50.0][0]],
-    m1.Flux[groups[25.0][0]:groups[50.0][0]],
-    m1.Freq[groups[25.0][0]:groups[50.0][0]],
-    m1.Hdc[groups[25.0][0]:groups[50.0][0]],
-    m1.duty_dif[groups[25.0][0]:groups[50.0][0]]
-    ))
-del m1.duty_dif, m1.Temp, m1.Flux, m1.Freq, m1.Hdc
+x = np.column_stack((
+    m1.Temp[r1:r2],
+    m1.Flux[r1:r2],
+    m1.Freq[r1:r2],
+    m1.Hdc[r1:r2],
+    m1.duty_dif[r1:r2],
+    m1.B_t[r1:r2, 0],
+    m1.B_t[r1:r2, 1],
+    m1.B_t[r1:r2, 2],
+    m1.B_t[r1:r2, 3]))
+del m1.duty_dif, m1.Temp, m1.Flux, m1.Freq, m1.Hdc, m1.B_t
 
-print("Beginning Optimization . . .") # divided into two sections to reduce weight randomization as precision increases
+y = np.column_stack((
+    m1.H_t[r1:r2, 0],
+    m1.H_t[r1:r2, 1],
+    m1.H_t[r1:r2, 2]))
+del m1.H_t
+
+
+print("Beginning Optimization. . .") # divided into two sections to reduce weight randomization as precision increases
 model1 = PySRRegressor(
     # warm_start = True,
     niterations = 10000000,
@@ -179,6 +203,5 @@ model2 = PySRRegressor(
         "exp": {"square": 1, "sin": 2, "cos": 2, "exp": 1},},
     turbo = True,
     elementwise_loss = "loss(prediction, target) = (prediction - target)^2",)
-# model1.fit(xdata, loss[groups[25.0][0]:groups[50.0][0]])
-# model2.fit(xdata, loss[groups[25.0][0]:groups[50.0][0]])
-
+model1.fit(x, y)
+model2.fit(x, y)
